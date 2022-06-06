92eb960d45e6ac3ade9d2fba0d3b44925d268c2d
LyubovTatur <71625191+LyubovTatur@users.noreply.github.com>
2022-06-06 13:23:26 +0300

    pixel design

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="content">
+    <ProductList :clothes="clothes" />
+  </div>
+</template>
+
+<script>
+import ProductList from "@/components/ProductList";
+export default {
+  components: {
+    ProductList,
+  },
+  data() {
+    return {
+      clothes: {},
+    };
+  },
+  methods: {
+    async getClothes() {
+      fetch("https://vlad-cloth-api.herokuapp.com/api/cloth/")
+        .then((response) => response.json())
+        .then((data) => (this.clothes = data));
+    },
+  },
+  mounted() {
+    this.getClothes();
+  },
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap");
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body {
+  background: url("https://images5.alphacoders.com/116/1168050.png") no-repeat center
+    center fixed;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+  -webkit-text-stroke-width: 1px;
+  -webkit-text-stroke-color: #020816;
+  font-family: "Press Start 2P", cursive;
+  color: #cb769b;
+}
+.content {
+  width: 649px;
+  margin: 10px auto;
+}
+</style>
---
+++ src/components/ProductList.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="product_list">
+    <div class="product_item" v-for="product_item in clothes">
+      <img class="product_item_img" src="" alt="item image" />
+      <h2 @mouseenter='testMethos()'>${{ product_item.price }}</h2>
+      <h3>{{ product_item.name }}</h3>
+      <div>{{ product_item.brand }}</div>
+    </div>
+  </div>
+  {{ clothes }}
+</template>
+
+<script>
+export default {
+  props: {
+    clothes: {
+      type: Array,
+      required: true,
+    },
+  },
+  methods:{
+      testMethos(){
+          console.log('mlem!')
+      }
+  }
+};
+</script>
+<style lang="scss">
+.product_list {
+  background: rgba(145, 5, 4, 0.2);
+    padding:3px;
+  .product_item {
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    display: inline-block;
+    margin: 3px;
+    border: 3px solid #020816;
+    width: 208px;
+    height: 300px;
+    h2,
+    h3,
+    div {
+      margin: 4px 4px;
+    }
+    h2{
+        // color: #e68ab3;
+        color: #020816;
+          -webkit-text-stroke-color: #cb769b;
+          -webkit-text-stroke-width: .1px;
+
+    }
+
+    .product_item_img {
+      margin: 1px;
+      min-width: 200px;
+      min-height: 200px;
+        border: 3px solid #020816;
+    }
+  }
+}
+</style>
